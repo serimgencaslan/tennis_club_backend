3a61349a7f90763fc78af300e4445c3ae3967f12
serimgencaslan <serim.123@hotmail.com>
2026-06-04 22:39:04 +0300

    add courts to db dosyasındaki db path i deiştirildi

--- add_courts_to_db.py
+++ add_courts_to_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('tennis.db') # Veritabanı dosya adınız farklıysa güncelleyin
+conn = sqlite3.connect('sqlite:////var/data/tennis.db') # Veritabanı dosya adınız farklıysa güncelleyin
 cursor = conn.cursor()
 
 courts = [('Kort 1', 'active'), ('Kort 2', 'active'), ('Kort 3', 'active')]
